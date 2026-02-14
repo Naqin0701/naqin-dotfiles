fdaf6c6066d80598dd1fb8c6d5584109ed68e5b8
Naqin <naqin0701@gmail.com>
2026-02-14 16:55:42 +0800

    Update: neovim -> nvim-cmp, which-key

---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,63 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp",
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+		},
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim", -- 美化补全菜单
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
+
+		require("luasnip.loaders.from_vscode").lazy_load() -- 加载 VSCode 风格片段
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-Space>"] = cmp.mapping.complete(), -- 触发补全
+				["<C-e>"] = cmp.mapping.close(), -- 关闭补全
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- 确认选择
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" }, -- LSP 源
+				{ name = "luasnip" },   -- 片段源
+				{ name = "buffer" }, -- 缓冲区源
+				{ name = "path" }, -- 路径源
+			}),
+			formatting = {
+				format = lspkind.cmp_format({ with_text = true, maxwidth = 50 }),
+			},
+		})
+	end,
+}
